890f68cc0ed559869e8a0331f5bf9d234d418b09
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-02 00:43:10 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Header from './Header/Header'
 
 
 
@@ -7,7 +8,7 @@ function App() {
 
     return (
         <>
-
+            <Header></Header>
         </>
     )
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-3xl text-center my-8">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
