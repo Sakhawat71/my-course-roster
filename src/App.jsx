bdce3a59fb4e7c320c4397f20c61e10b0e4e0a28
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-03 03:23:08 +0600

    caltulating complete and the data is shown.

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,46 @@ import Header from './components/Header/Header'
 
 function App() {
 
-    // const [hrRemain ,setHrRemain] = useState(20);
-    // const [courseNames,setCourseNames] = useState([]);
+    const [creditRemain, setCreditRemain] = useState(20);
+    const [credit, setCredit] = useState(0);
     const [price, setPrice] = useState(0);
     const [selectedCourses, setSelectedCourses] = useState([]);
 
     const handelSelect = (course) => {
+        // setCredit(course.credit)
         const isExist = selectedCourses.find(item => item.id === course.id);
-        console.log(course.price)
+        console.log(course.credit, 'clicked totalCredit')
+        
         let totalPrice = course.price;
+        let totalCredit = course.credit;
 
-        if (isExist) {
+        if (isExist ) {
             return alert('exist');
         }
         else {
-            setSelectedCourses([...selectedCourses, course]);
-        }
-        selectedCourses.forEach(item => {
-            totalPrice += item.price;
-        })
-        setPrice(totalPrice)
-    }
+            
+            selectedCourses.forEach(item => {
+                totalPrice += item.price;
+                totalCredit = totalCredit + item.credit;
+                console.log(course.credit, 'inside foreach')
+            })
+            if (totalCredit > 20) {
+                return alert("you can not take more then 20 credit");
+            }
+            else {
+                setPrice(totalPrice)
+                setCredit(totalCredit)
+                setCreditRemain(20 - totalCredit)
+            }
 
+            setSelectedCourses([...selectedCourses, course]);
+            
+        }
+        
+        console.log(totalCredit, 'totalc')
+    }
+    console.log(credit,'state')
+    
 
 
 
@@ -42,13 +60,13 @@ function App() {
 
                 <Courses
                     handelSelect={handelSelect}
-
                 ></Courses>
-
 
                 <Cart
                     courses={selectedCourses}
                     price={price}
+                    credit={credit}
+                    creditRemain={creditRemain}
                 ></Cart>
 
             </div>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-const Cart = ({ courses , price}) => {
+const Cart = ({ courses , price , credit, creditRemain}) => {
 
     return (
         <div className="w-3/12 bg-white p-6 space-y-4 rounded-md">
-            <h1 className='text-[#2F80ED] font-bold text-[18px]'>Credit Hour Remaining {} hr</h1>
+            <h1 className='text-[#2F80ED] font-bold text-[18px]'>Credit Hour Remaining {creditRemain} hr</h1>
             <hr />
 
             <div>
@@ -16,7 +16,7 @@ const Cart = ({ courses , price}) => {
                 </ul>
             </div>
             <hr />
-            <h4 className='text-[16px] font-medium'>Total Credit Hour : {}</h4>
+            <h4 className='text-[16px] font-medium'>Total Credit Hour : {credit}</h4>
             <hr />
             <h3 className='font-semibold text-xl'>Total Price : {price} USD</h3>
         </div>
@@ -26,6 +26,8 @@ const Cart = ({ courses , price}) => {
 Cart.propTypes = {
     courses: PropTypes.array.isRequired,
     price: PropTypes.number,
+    credit: PropTypes.number,
+    creditRemain: PropTypes.number
 };
 
 export default Cart;
