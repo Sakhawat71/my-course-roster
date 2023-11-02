b08a4856271d6abf1d3455084b66d536c07236e8
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-03 04:36:37 +0600

    react-toastify used for show worning

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ live link:
 https://course-regi-sabbir71.netlify.app/
 
 https://course-regi-sabbir71.surge.sh/
+
+
+
+
+### Features
+
+- Courses cannot be taken for a maximum of 20 credits.
+- A course may not be taken more than once.
+- The course is made with react.js.
+- This project has been deployed on surge and netlify.
+- Tailwind CSS is used for style and react-toastify is used to give a warning.
+
+
+
+# Q: Discuss how you managed the state in your assignment project.
+A: I used it to create and update variables that held information I needed to display and interact with in my components. This allowed me to keep track of things like user input, component visibility, and more, making my project dynamic and responsive.
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import Cart from './components/Cart/Cart'
 import Courses from './components/Courses/Courses'
 import Header from './components/Header/Header'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -14,25 +15,43 @@ function App() {
     const [selectedCourses, setSelectedCourses] = useState([]);
 
     const handelSelect = (course) => {
-        // setCredit(course.credit)
+
         const isExist = selectedCourses.find(item => item.id === course.id);
-        console.log(course.credit, 'clicked totalCredit')
-        
+
         let totalPrice = course.price;
         let totalCredit = course.credit;
 
-        if (isExist ) {
-            return alert('exist');
+        if (isExist) {
+            toast.warn('This course exists!', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            return;
         }
         else {
-            
+
             selectedCourses.forEach(item => {
                 totalPrice += item.price;
                 totalCredit = totalCredit + item.credit;
-                console.log(course.credit, 'inside foreach')
             })
             if (totalCredit > 20) {
-                return alert("you can not take more then 20 credit");
+                toast.warn("😢 Limited to 20 credits!",{
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                return;
             }
             else {
                 setPrice(totalPrice)
@@ -41,15 +60,10 @@ function App() {
             }
 
             setSelectedCourses([...selectedCourses, course]);
-            
+
         }
-        
-        console.log(totalCredit, 'totalc')
+
     }
-    console.log(credit,'state')
-    
-
-
 
 
 
@@ -68,7 +82,7 @@ function App() {
                     credit={credit}
                     creditRemain={creditRemain}
                 ></Cart>
-
+                <ToastContainer></ToastContainer>
             </div>
         </>
     )
